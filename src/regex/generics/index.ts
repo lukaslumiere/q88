57fc80d18c16497749de91ccb0dbf1bc5e277043
lukas <lukaswp.dzn@gmail.com>
2025-10-05 00:15:56 -0300

    feat(regex): add rfc and generic regex utilities

---
+++ src/regex/generics/index.ts
@@ -0,0 +1,3 @@
+export * from "./number";
+export * from "./special-char";
+export * from "./uppercase";
---
+++ src/regex/rfc/index.ts
@@ -0,0 +1,6 @@
+// these regexes follow the rules set by the relevant rfcs
+// rfcs are official documents that define how things like emails, urls, and dates should be formatted
+
+export * from "./date-time";
+export * from "./email";
+export * from "./url";
